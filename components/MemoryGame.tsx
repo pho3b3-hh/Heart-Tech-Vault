f17ae2adee4b9be54f1b4aa01e69f095e6017ba3
pho3b3-hh <phoebe12345640@gmail.com>
2026-02-11 12:52:14 +0100

    Fix image paths in MemoryGame component

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -7,14 +7,14 @@ interface MemoryGameProps {
 }
 
 const IMAGES = [
-  '/pic1.jpg', 
-  '/pic2.jpg',
-  '/pic3.jpg',
-  '/pic4.jpe',
-  '/pic5.jpg',
-  '/pic6.jpg',
-  '/pic7.jpg',
-  '/pic8.jpg',
+  'pic1.jpg', 
+  'pic2.jpg',
+  'pic3.jpg',
+  'pic4.jpe',
+  'pic5.jpg',
+  'pic6.jpg',
+  'pic7.jpg',
+  'pic8.jpg',
 ];
 
 const MemoryGame: React.FC<MemoryGameProps> = ({ onComplete }) => {
